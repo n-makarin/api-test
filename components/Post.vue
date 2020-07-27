4edccb67cee2763ad318c9f6b62fd97a5e7d2707
Nikita Makarin <zn.makarin@gmail.com>
2020-07-27 20:08:24 +0300

    Create components/Post.vue

---
+++ components/Post.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="post">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped lang="scss">
+
+</style>
