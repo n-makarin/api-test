0f2494f2ac73392f6168b69069810ae02ac72e7f
Nikita Makarin <zn.makarin@gmail.com>
2020-07-27 20:29:10 +0300

    Crate pages/post/_id.vue

---
+++ pages/post/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">
+    {{ $route.params }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
