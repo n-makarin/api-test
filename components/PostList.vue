a3de2c5089ba2479bbfb368fb5a42d9973825429
Nikita Makarin <zn.makarin@gmail.com>
2020-07-28 10:11:03 +0300

    Add Post to PostList

--- components/PostList.vue
+++ components/PostList.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="post-list">
+    <div
+      v-for="post in data"
+      :key="post.id"
+      class="post-list__wrapper"
+    >
+      <Post
+        :data="post"
+        brief
+        class="post-list__item"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { IPost } from '~/types/post'
+import Post from '~/components/Post.vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {
+    Post
+  },
+
+  props: {
+    data: {
+      type: Array,
+      default: (): IPost[] => []
+    }
+  }
+})
 </script>
 
 <style scoped lang="scss">
