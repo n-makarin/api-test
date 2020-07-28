d5f804fb64f25d9feab339f8b777ae2aa4dfa908
Nikita Makarin <zn.makarin@gmail.com>
2020-07-28 10:10:17 +0300

    Add fetch method to post-list page

--- pages/post-list.vue
+++ pages/post-list.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="index">
     <h1>Blog</h1>
-    <PostList />
+    <PostList :data="data" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import PostList from '~/components/PostList.vue'
+import { IPost } from '~/types/post'
 
 export default Vue.extend({
   components: {
     PostList
+  },
+
+  async fetch () {
+    await this.$store.dispatch('post-list/fetchData')
+
+    this.data = this.$store.getters['post-list/data']
+  },
+
+  data (): {
+    data: IPost[]
+    } {
+    return {
+      data: []
+    }
   }
 })
 </script>
