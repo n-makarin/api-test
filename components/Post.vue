e4fb47a1863c90aa457831980d860003f4fb8043
Nikita Makarin <zn.makarin@gmail.com>
2020-07-28 10:10:47 +0300

    Add markup & props to Post

--- components/Post.vue
+++ components/Post.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="post">
+    <div class="post__title">
+      {{ data.title }}
+    </div>
+    <div
+      v-if="brief"
+      class="post__description"
+    >
+      {{ data.description }}
+    </div>
+    <div
+      v-else
+      class="post__text"
+    >
+      {{ data.text }}
+    </div>
+    <div class="post__date">
+      {{ data.date_of_creation }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  props: {
+    data: {
+      type: Object,
+      default: () => {}
+    },
+    brief: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
 </script>
 
 <style scoped lang="scss">
