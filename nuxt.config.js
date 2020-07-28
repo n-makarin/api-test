d8a038744b6b89b8757e0aa78afa7ed7b0a1b5a2
Nikita Makarin <zn.makarin@gmail.com>
2020-07-28 09:22:33 +0300

    Add webpack autofix prop

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,5 +64,17 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
+    extend (config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue|ts)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: { fix: true }
+        })
+      }
+    }
   }
 }
